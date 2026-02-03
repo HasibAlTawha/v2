95290c422f42db50e9081990b09559c1e1d39b9b
Hasib Al Tawha ツ <57056025+HasibAlTawha@users.noreply.github.com>
2026-02-03 20:13:28 +0600

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg"
+              src="https://i.postimg.cc/br0rV8Bb/IMG-3570.jpg"
               alt="Hasib portrait"
               width="192"
               height="192"
@@ -62,7 +62,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Hasib.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Hasib Al Tawha.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">4 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://linkedin.com/in/HasibAlTawha"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/HasibAlTawha"
           target="_blank"
         >
           <FaGithubSquare />
