7f03e9f164ea25536fc04ddab99908783e1b8fe9
Hasib Al Tawha ツ <57056025+HasibAlTawha@users.noreply.github.com>
2026-02-04 11:47:19 +0600

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <mdhasiburrahmanhsb@gmail.com>",
-      to: "hasibaltawha@gmail.com",
+      to: "mdhasiburrahmanhsb@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
